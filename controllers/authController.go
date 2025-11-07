73235feef6cc5d64f920a194fb5830b13d78414f
jayced1 <jesseeren@gmail.com>
2025-11-07 16:19:13 +0200

    jwt implementation

--- controllers/authController.go
+++ controllers/authController.go
@@ -3,10 +3,20 @@ package controllers
 import (
 	"go-auth/database"
 	"go-auth/database/models"
+	"strconv"
+	"time"
 
 	"github.com/gofiber/fiber/v2"
+	"github.com/golang-jwt/jwt/v5"
 	"golang.org/x/crypto/bcrypt"
 )
+
+type JWTClaims struct {
+	UserID uint   `json:"user_id"`
+	Email  string `json:"email"`
+	Name   string `json:"name"`
+	jwt.RegisteredClaims
+}
 
 func Register(c *fiber.Ctx) error {
 	var data map[string]string
@@ -47,5 +57,26 @@ func Login(c *fiber.Ctx) error {
 			"message": "Invalid credentials",
 		})
 	}
-	return c.JSON(user)
+
+	claims := jwt.NewWithClaims(jwt.SigningMethodHS256, JWTClaims{
+		UserID: user.Id,
+		Email:  user.Email,
+		Name:   user.Name,
+		RegisteredClaims: jwt.RegisteredClaims{
+			Issuer:    "go-auth",
+			Subject:   strconv.Itoa(int(user.Id)),
+			Audience:  []string{"go-auth"},
+			ExpiresAt: jwt.NewNumericDate(time.Now().Add(24 * time.Hour)),
+			IssuedAt:  jwt.NewNumericDate(time.Now()),
+		},
+	})
+
+	token, err := claims.SignedString([]byte("secret"))
+
+	if err != nil {
+		return c.Status(fiber.StatusInternalServerError).JSON(fiber.Map{
+			"message": "Could not log in",
+		})
+	}
+	return c.JSON(token)
 }
